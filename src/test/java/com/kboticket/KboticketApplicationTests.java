74735d0e2ad3a54b2008e57fb520776360824f15
byunyourim <byourim@google.com>
2024-04-28 02:45:25 +0900

    kboticket

---
+++ src/test/java/com/kboticket/KboticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kboticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KboticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
